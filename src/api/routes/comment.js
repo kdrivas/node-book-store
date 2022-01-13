ebf52889ada1ce30187302299f7fd472fb378418
kervyRivas <a20112128@pucp.pe>
2022-01-12 21:54:47 -0500

    feat: creating comment create route

--- src/api/routes/comment.js
+++ src/api/routes/comment.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import isAuth from "../middlewars/isAuth";
 
 const router = Router();
 
@@ -7,5 +8,14 @@ export default (app) => {
 
   app.get('/', (req, res, next) => {
     return res.send(200);
+  })
+
+  app.post('/:postId', isAuth, async (req, res, next) => {
+    try {
+      const comment = await createComment(req)
+      res.status(200).json({ comment })
+    } catch (e) {
+      next(e)
+    }
   })
 }
